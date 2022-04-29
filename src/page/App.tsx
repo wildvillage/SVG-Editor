c8e2de75b60e5cdefc4c981c91062542662eea58
wildvillage <wysmusic@163.com>
2022-04-30 00:23:57 +0800

    ci(lint): add lint

---
+++ src/page/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>click</Button>
+    </div>
+  );
+}
+
+export default App;
