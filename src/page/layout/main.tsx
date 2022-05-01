cfe9ec333ff8c9280a511120b905f0f2949faa17
songxiaopeng <songxiaopeng2021@163.com>
2022-05-01 14:05:37 +0800

    feat:合并main

--- src/page/layout/main.tsx
+++ src/page/layout/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './index.module.less';
+import Store from '../store';
 
 const Header: React.FC = () => {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <Store></Store>
+    </div>
+  );
 };
 
 export default Header;
--- src/page/layout/slider.tsx
+++ src/page/layout/slider.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import styles from './index.module.less';
+import { ExpandOutlined } from '@ant-design/icons';
+import { setting } from '../../settings/action';
+import { InputNumber } from 'antd';
+import { render } from 'react-dom';
+
+const current = 'circle';
 
 const Slider: React.FC = () => {
-  return <div className={styles.slider}></div>;
+  const currentSetting = setting[current];
+
+  const actions = [
+    {
+      title: '几何位置',
+      key: 'default',
+    },
+    {
+      title: 'svg属性',
+      key: 'props',
+    },
+    {
+      title: '非几何属性',
+      key: 'notGeometric',
+    },
+  ];
+
+  return (
+    <div className={styles.slider}>
+      <div className={styles.logo}>
+        <ExpandOutlined style={{ fontSize: '20px' }} />
+        <span className={styles.title}>svg editor</span>
+      </div>
+      <div className={styles['action-area']}>
+        {actions.map((item, i) => {
+          return <div key={i} className={styles.card}></div>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
