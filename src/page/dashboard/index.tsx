7752b38afc517d46aa8546b3e599afa4352d02dc
wildvillage <wysmusic@163.com>
2022-05-01 20:51:09 +0800

    feat(scale): create splitline

--- src/page/dashboard/index.tsx
+++ src/page/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useMemo, RefObject } from 'react';
 import { useDashboardSize } from '../../utils';
 import { SVG_XMLNS } from '../../index';
 import styles from './index.module.less';
@@ -9,8 +9,28 @@ function Dashboard() {
 
   return (
     <div className={styles.dashboard} ref={dashboard}>
+      {/* 网格线 */}
+      <SplitLine
+        width={dashboard.current?.offsetWidth as number}
+        height={dashboard.current?.offsetHeight as number}
+      />
       <svg xmlns={SVG_XMLNS}></svg>
     </div>
+  );
+}
+
+function SplitLine({ width, height }: { width: number; height: number }) {
+  const splitLine = useMemo(() => {
+    if (height && width) {
+      const vertical = Math.ceil(width / 100);
+      // const scaleTopLine =
+    }
+  }, [height, width]);
+
+  return (
+    <svg xmlns={SVG_XMLNS} className={styles.splitLine}>
+      asd
+    </svg>
   );
 }
 
